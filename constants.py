8809d7b8d26a46bedf9e0d24e2a6ebbcb33e1d7e
misterlister <112644979+misterlister@users.noreply.github.com>
2024-04-18 12:11:27 -0700

    Moved constants to separate file

---
+++ constants.py
@@ -0,0 +1,62 @@
+from enum import IntEnum
+import os.path
+
+class FoodNum(IntEnum):
+    # number keys for each type of food
+    GLOW = 1
+    SLOW = 2
+    BONUS = 3
+    MYSTERY = 4
+    SHIELD = 5
+    NORMAL = 0
+
+class Direction(IntEnum):
+    UP = 0
+    DOWN = 180
+    LEFT = 90
+    RIGHT = 270
+
+MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
+SPRITES_DIR = os.path.join(MAIN_DIR, "sprites")
+
+BLACK_COL = (0, 0, 0) #colour for snake
+BACKGROUND_COL = (205, 225, 255) #colour for the main background screen
+GREEN_COL = (0, 155, 0) #colour for normal food
+RED_COL = (255, 0, 0) #colour for game over text
+WHITE_COL = (255, 255, 255) #colour for the background of the game over screen
+YELLOW_COL = (255, 215, 0) #colour for score and food effect text
+HEADER_COL = (0, 0, 150) #colour for the header background
+TRANSPARENT = (69, 69, 69) #colour to be used for transparency
+
+DIS_WIDTH = 1280
+DIS_HEIGHT = 920
+
+HEADER_HEIGHT = 55
+LINE_WIDTH = 3
+
+PLAY_HEIGHT = DIS_HEIGHT-HEADER_HEIGHT
+
+CLOCK_SPEED = 40
+
+SPRITE_SCALE = 1.25
+SEG_LENGTH = 20 * SPRITE_SCALE
+START_SPEED = 4
+MIN_SPEED = 3
+# How many food objects will spawn
+NUM_FOOD = 3
+
+MESSAGE_DURATION_MAX = 90
+BLINDNESS_TIME_MAX = 750
+BLINDNESS_TIME_PHASE = BLINDNESS_TIME_MAX / 10
+
+TAIL_RADIUS = 1/2 * SEG_LENGTH
+COLLISION_RADIUS = 3/4 * SEG_LENGTH
+SAFE_RADIUS = 5 * SEG_LENGTH
+
+# rate at which speed changes when eating speed changing food
+SPEED_INC = 1/2
+
+SAVE_FILE_NAME = "highscores.txt"
+
+SHADOW_OFFSET = 2
+
