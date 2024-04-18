cdbb57010912658ad545b23e4d0fc1bab9689bc4
misterlister <112644979+misterlister@users.noreply.github.com>
2024-04-18 12:32:02 -0700

    changed main file from SnakeGame to main

---
+++ main.py
@@ -0,0 +1,13 @@
+# 
+# Project Name: Snake
+#
+# Designed and Developed by Hayden Lister
+#
+
+from game_loop import gameLoop
+
+def main():
+    gameLoop()
+
+if __name__ == "__main__":
+    main()
